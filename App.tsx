6812cea01cab62a2051a177ecc74fe80fc956009
Sherzod <sherzodjonsadillaev@gmail.com>
2024-09-25 19:59:32 +0500

    firebase 🔥

--- App.tsx
+++ App.tsx
@@ -1,23 +1,61 @@
-import { Provider } from "react-redux";
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import { enableScreens } from "react-native-screens";
-import store from "./src/store/configureStore";
-import Root from "./src/navigation/Root";
-import React from "react";
-import "./ignoreWarnings";
-enableScreens();
+import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import { enableScreens } from 'react-native-screens'
+import store from './src/store/configureStore'
+import Root from './src/navigation/Root'
+import React, { useEffect } from 'react'
+import messaging from '@react-native-firebase/messaging'
 
-const persistor = persistStore(store);
+enableScreens()
+
+const persistor = persistStore(store)
+
+// Запрос разрешений для iOS
+const requestUserPermission = async () => {
+	const authStatus = await messaging().requestPermission()
+	const enabled =
+		authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+		authStatus === messaging.AuthorizationStatus.PROVISIONAL
+
+	if (enabled) {
+		console.log('Уведомления разрешены')
+	}
+}
+
+// Получение токена устройства
+const getDeviceToken = async () => {
+	const token = await messaging().getToken()
+	console.log('FCM Token:', token)
+}
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Root />
-      </PersistGate>
-    </Provider>
-  );
-};
+	useEffect(() => {
+		// Запрос разрешений при запуске приложения
+		requestUserPermission()
 
-export default App;
+		// Получение FCM токена
+		getDeviceToken()
+
+		// Обработчик для сообщений, полученных в фоне
+		messaging().setBackgroundMessageHandler(async remoteMessage => {
+			console.log('Сообщение в фоне:', remoteMessage)
+		})
+
+		// Обработчик для сообщений, полученных, когда приложение активно
+		const unsubscribe = messaging().onMessage(async remoteMessage => {
+			console.log('Сообщение в активном приложении:', remoteMessage)
+		})
+
+		return unsubscribe
+	}, [])
+	return (
+		<Provider store={store}>
+			<PersistGate persistor={persistor}>
+				<Root />
+			</PersistGate>
+		</Provider>
+	)
+}
+
+export default App
